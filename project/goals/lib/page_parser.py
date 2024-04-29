232a58bdb9db1a5d6c1934ada63d6bcc6a2efcc0
nezinomas <nezinomas@gmai.com>
2024-04-29 11:05:30 +0300

    rewrite page_parser to parse leaderboard table instead strava feed

--- project/goals/lib/page_parser.py
+++ project/goals/lib/page_parser.py
@@ -1,7 +1,3 @@
-import contextlib
-import re
-from datetime import datetime, timedelta
-
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet
 
@@ -20,19 +16,11 @@ class PageParser:
             ResultSet: A collection of items found in the page.
         """
         content = BeautifulSoup(page, "html.parser")
-        return content.find_all("div", id=lambda x: x and x.startswith("feed-entry-"))
+        table = content.find_all("table")
+        if not table:
+            return None
 
-    def get_entry_id(self, item: ResultSet) -> int:
-        """
-        Parses the entry id from a given feed item.
-        Args:
-            item (bs4.element.ResultSet): The feed item to parse.
-        Returns:
-            int: The entry id.
-        """
-        pattern = re.compile(r'feed-entry-(\d+)')
-        res = re.findall(pattern, str(item))
-        return int(res[0])
+        return table[0].find_all("tbody")[0].find_all("tr")
 
     def get_strava_id(self, item: ResultSet) -> int:
         """
@@ -42,7 +30,7 @@ class PageParser:
         Returns:
             int: The strava id.
         """
-        link = item.find("a", attrs={"data-testid": "owners-name"})["href"]
+        link = item.find("a", {"class": "athlete-name"})["href"]
         # href="/athletes/5577666"
         return int(link.split("/")[-1])
 
@@ -54,54 +42,7 @@ class PageParser:
         Returns:
             str: The strava athlete name.
         """
-        return item.find("a", attrs={"data-testid": "owners-name"}).text
-
-    def get_date(self, item: ResultSet) -> datetime:
-        """
-        Parses the date from a given feed item.
-        Args:
-            item (bs4.element.ResultSet): The feed item to parse.
-        Returns:
-            datetime: The date and time.
-        """
-        date_str = item.find("time", attrs={"data-testid": "date_at_time"}).text
-
-        if "Today" in date_str:
-            date_str = date_str.replace("Today", datetime.now().strftime("%B %d, %Y"))
-
-        if "Yesterday" in date_str:
-            date_str = date_str.replace(
-                "Yesterday",
-                (datetime.now() - timedelta(days=1)).strftime("%B %d, %Y"),
-            )
-
-        formats = [
-            "%d %B %Y",
-            "%d %B %Y at %H:%M",
-            "%B %d %Y",
-            "%B %d, %Y",
-            "%B %d, %Y at %I:%M %p",
-            "%B %d, %Y at %I:%M",
-            "%B %d, %Y at %H:%M"
-        ]
-
-        for fmt in formats:
-            with contextlib.suppress(ValueError):
-                return datetime.strptime(date_str, fmt)
-
-        raise ValueError(f"Unrecognized datetime format {date_str}")
-
-    def get_activity(self, item: ResultSet) -> str:
-        """
-        Parses the activity from a given feed item.
-        Args:
-            item (bs4.element.ResultSet): The feed item to parse.
-        Returns:
-            str: The activity name.
-        """
-        activity_type_str = item.find("title")
-
-        return "Workout" if activity_type_str is None else activity_type_str.text
+        return item.find("a", {"class": "athlete-name"}).text
 
     def get_distance(self, item: ResultSet) -> int:
         """
@@ -111,18 +52,14 @@ class PageParser:
         Returns:
             int: The distance in meters.
         """
-        distance_str = item.find("span", string="Distance")
-        if distance_str is None:
+        distance_str = item.find("td", {"class": "distance"}).text
+
+        try:
+            distance, _ = distance_str.split(" ")
+        except ValueError:
             return 0
 
-        distance, units = distance_str.next_sibling.text.split(" ")
-
-        if units == "m":
-            distance = distance.split(",")[0]
-        else:
-            distance = float(distance) * 1000
-
-        return int(distance)
+        return int(float(distance) * 1000)
 
     def get_time(self, item: ResultSet) -> int:
         """
@@ -134,7 +71,7 @@ class PageParser:
         """
         total_seconds = 0
 
-        time_string = item.find("span", string="Time").next_sibling.text
+        time_string = item.find("td", {"class": "moving_time"}).text
         time_components = time_string.split()
 
         for component in time_components:
@@ -156,8 +93,11 @@ class PageParser:
         Returns:
             int: The ascent in meters.
         """
-        ascent_str = item.find("span", string="Elev Gain")
-        if ascent_str is None:
+        ascent_str = item.find("td", {"class": "elev_gain"}).text
+
+        try:
+            ascent, _ = ascent_str.split(" ")
+        except ValueError:
             return 0
 
-        return int(ascent_str.next_sibling.text.split(" ")[0])
+        return int(ascent)
--- project/goals/tests/test_lib_page_parser.py
+++ project/goals/tests/test_lib_page_parser.py
@@ -1,21 +1,22 @@
-from datetime import datetime
-
 import pytest
 from bs4 import BeautifulSoup
-import time_machine
+
 from ..lib.page_parser import PageParser
 
 
 def test_get_items():
     page = """
-    <html>
-        <body>
-            <main>
-                <div id="feed-entry-1"></div>
-                <div id="feed-entry-2"></div>
-            </main>
-        </body>
-    </html>
+    <div class="leaderboard">
+        <table  class="dense striped sortable">
+            <thead>
+                <tr><th></th></tr>
+            </thead>
+            <tbody>
+                <tr><td></td></tr>
+                <tr><td></td></tr>
+            </tbody>
+        </table>
+    </div>
     """
 
     obj = PageParser(page)
@@ -23,50 +24,46 @@ def test_get_items():
     assert len(obj.items) == 2
 
 
-def test_strava_entry_id():
-    item = BeautifulSoup('<div class="------packages-feed-ui-src-features-FeedEntry__entry-container--FPn3K" id="feed-entry-123456789" role="button" style="" tabindex="0"></div>', "html.parser")
-
-    entry_id = PageParser("").get_entry_id(item)
-    assert entry_id == 123456789
-
-
 @pytest.mark.parametrize(
-    "value, unit, expected",
+    "value, expect",
     [
-        ("12.09", "km", 12090),
-        ("2,075", "m", 2),
+        ("0.6", 600),
+        ("1.6", 1600),
     ],
 )
-def test_get_distance(value, unit, expected):
+def test_get_distance(value, expect):
     item = BeautifulSoup(
-        f'<li><div class="------packages-ui-Stat-Stat-module__stat--Y2ZBX"><span class="------packages-ui-Stat-Stat-module__statLabel--tiWBB ------packages-feed-ui-src-components-ActivityEntryBody-ActivityEntryBody__stat-label--DjJOy">Distance</span><div class="------packages-ui-Stat-Stat-module__statValue--phtGK">{value}<abbr class="unit" title="kilometers"> {unit}</abbr></div></div></li>',
+        f'<td class="distance">{value} <abbr class="unit short" title="kilometers">km</abbr></td>',
         "html.parser",
     )
 
     distance = PageParser("").get_distance(item)
-    assert distance == expected
+    assert distance == expect
 
 
-def test_get_distance_not_found():
-    item = BeautifulSoup("<li></li>", "html.parser")
+def test_get_distance_empty():
+    item = BeautifulSoup(
+        '<td class="distance">--</td>',
+        "html.parser",
+    )
 
     distance = PageParser("").get_distance(item)
     assert distance == 0
 
 
-def test_get_time_minutes_seconds():
+def test_get_time_minutes():
     item = BeautifulSoup(
-        '<div class="------packages-ui-Stat-Stat-module__stat--Y2ZBX"><span class="------packages-ui-Stat-Stat-module__statLabel--tiWBB ------packages-feed-ui-src-components-ActivityEntryBody-ActivityEntryBody__stat-label--DjJOy">Time</span><div class="------packages-ui-Stat-Stat-module__statValue--phtGK">1<abbr class="unit" title="minute">m</abbr> 2<abbr class="unit" title="second">s</abbr></div></div>',
+        '<td class="moving_time highlighted-column">1<abbr class="unit" title="minute">m</abbr></td>',
         "html.parser",
     )
 
     time = PageParser("").get_time(item)
-    assert time == 62
+    assert time == 60
 
 
 def test_get_time_hours_minutes():
     item = BeautifulSoup(
-        '<div class="------packages-ui-Stat-Stat-module__stat--Y2ZBX"><span class="------packages-ui-Stat-Stat-module__statLabel--tiWBB ------packages-feed-ui-src-components-ActivityEntryBody-ActivityEntryBody__stat-label--DjJOy">Time</span><div class="------packages-ui-Stat-Stat-module__statValue--phtGK">1<abbr class="unit" title="hour">h</abbr> 2<abbr class="unit" title="minute">m</abbr></div></div>',
+        '<td class="moving_time highlighted-column">1<abbr class="unit" title="hour">h</abbr> 2<abbr class="unit" title="minute">m</abbr></td>',
         "html.parser",
     )
 
@@ -74,71 +71,41 @@ def test_get_time_hours_minutes():
     assert time == 3720
 
 
-def test_get_activity():
-    item = BeautifulSoup(
-        '<div class="------packages-feed-ui-src-features-Activity-Activity__entry-icon--hn2AC"><svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="------packages-feed-ui-src-features-Activity-Activity__activity-icon--lq3sA"><title>Walk</title><g fill=""><path d="M8.33"></path></g></svg></div>',
-        "html.parser",
-    )
-
-    activity = PageParser("").get_activity(item)
-    assert activity == "Walk"
-
-
 def test_get_ascent():
     item = BeautifulSoup(
-        '<div class="------packages-ui-Stat-Stat-module__stat--Y2ZBX"><span class="------packages-ui-Stat-Stat-module__statLabel--tiWBB ------packages-feed-ui-src-components-ActivityEntryBody-ActivityEntryBody__stat-label--DjJOy">Elev Gain</span><div class="------packages-ui-Stat-Stat-module__statValue--phtGK">68<abbr class="unit" title="meters"> m</abbr></div></div>',
+        '<td class="elev_gain">117 <abbr class="unit short" title="meters">m</abbr></td>',
         "html.parser",
     )
 
     ascent = PageParser("").get_ascent(item)
-    assert ascent == 68
+    assert ascent == 117
+
+
+def test_get_ascent_empty():
+    item = BeautifulSoup(
+        '<td class="elev_gain">--</td>',
+        "html.parser",
+    )
+
+    ascent = PageParser("").get_ascent(item)
+    assert ascent == 0
 
 
 def test_get_strava_id():
     item = BeautifulSoup(
-        '<div class="------packages-feed-ui-src-components-HeaderWithOwnerMetadata-HeaderWithOwnerMetadata-module__nameBoosted--ejkqR"><a href="/athletes/5577666" data-testid="owners-name">UserName</a></div>',
+        '<a class="athlete-name minimal" href="/athletes/123456789">UserName</a>',
         "html.parser",
     )
 
     strava_id = PageParser("").get_strava_id(item)
-    assert strava_id == 5577666
+    assert strava_id == 123456789
 
 
 def test_get_name():
     item = BeautifulSoup(
-        '<div class="------packages-feed-ui-src-components-HeaderWithOwnerMetadata-HeaderWithOwnerMetadata-module__nameBoosted--ejkqR"><a href="/athletes/5577666" data-testid="owners-name">UserName</a></div>',
+        '<a class="athlete-name minimal" href="/athletes/123456789">UserName</a>',
         "html.parser",
     )
 
     name = PageParser("").get_name(item)
     assert name == "UserName"
-
-
-@pytest.mark.parametrize(
-    "value, expect",
-    [
-        ("21 April 2024", datetime(2024, 4, 21, 0, 0, 0)),
-        ("21 April 2024 at 15:00", datetime(2024, 4, 21, 15, 0, 0)),
-        ("April 21, 2024", datetime(2024, 4, 21, 0, 0, 0)),
-        ("April 21 2024", datetime(2024, 4, 21, 0, 0, 0)),
-        ("April 21, 2024 at 06:05 AM", datetime(2024, 4, 21, 6, 5, 0)),
-        ("April 21, 2024 at 06:05 PM", datetime(2024, 4, 21, 18, 5, 0)),
-        ("April 21, 2024 at 06:05", datetime(2024, 4, 21, 6, 5, 0)),
-        ("April 21, 2024 at 18:05", datetime(2024, 4, 21, 18, 5, 0)),
-        ("Today", datetime(2022, 4, 25, 0, 0, 0)),
-        ("Today at 6:36 AM", datetime(2022, 4, 25, 6, 36, 0)),
-        ("Today at 6:36 PM", datetime(2022, 4, 25, 18, 36, 0)),
-        ("Yesterday", datetime(2022, 4, 24, 0, 0, 0)),
-        ("Yesterday at 6:36 AM", datetime(2022, 4, 24, 6, 36, 0)),
-        ("Yesterday at 6:36 PM", datetime(2022, 4, 24, 18, 36, 0)),
-    ],
-)
-@time_machine.travel("2022-04-25")
-def test_get_date(value, expect):
-    item = BeautifulSoup(
-        f'<time data-testid="date_at_time">{value}</time>', "html.parser"
-    )
-
-    date = PageParser("").get_date(item)
-
-    assert date == expect
