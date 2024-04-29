5aa7ca94153cd7c4ac6f1cb0eb7eb258910f476c
nezinomas <nezinomas@gmai.com>
2024-04-29 16:39:31 +0300

    update return object type, dictionary instead list

--- project/goals/managers.py
+++ project/goals/managers.py
@@ -21,25 +21,26 @@ class EntryManager(models.QuerySet):
         return self.select_related("athlete")
 
     def week_stats(self, date: pendulum.Date):
-        start = date.start_of('week')
-        end = date.end_of('week')
+        start = date.start_of("week")
+        end = date.end_of("week")
 
         qs = (
             self.related()
             .filter(date__gte=start, date__lte=end)
             .annotate(cnt=Count("athlete__strava_id"))
             .values("athlete__strava_id")
-            .annotate(moving_time=Sum("moving_time"), num_activities=Sum("num_activities"))
+            .annotate(
+                moving_time=Sum("moving_time"),
+                num_activities=Sum("num_activities")
+            )
             .order_by("athlete__strava_id")
             .values("athlete__strava_id", "moving_time", "num_activities")
         )
 
-        return [
-            {
-                str(entry["athlete__strava_id"]): {
-                    "moving_time": entry["moving_time"],
-                    "num_activities": entry["num_activities"],
-                }
+        return {
+            entry["athlete__strava_id"]: {
+                "moving_time": entry["moving_time"],
+                "num_activities": entry["num_activities"],
             }
             for entry in qs
-        ]
+        }
