6d1b6ad1dd25c3de1d1f4adfde78cd599c188e67
nezinomas <nezinomas@gmai.com>
2024-04-30 14:55:10 +0300

    convert seconds and meters filters

---
+++ project/goals/templatetags/filters.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def convert_seconds(seconds):
+    hours = seconds // (60*60)
+    seconds %= (60*60)
+    minutes = seconds // 60
+
+    hours = f"{hours}h" if hours else ""
+    minutes = f"{minutes}m" if minutes else "0m"
+
+    return f"{hours} {minutes}" if hours and minutes else minutes
+
+
+@register.filter
+def convert_meters(meters):
+    return f"{(meters / 1000):.1f}km"
