6c49acec30f6914acedb12e0701a22265a02ba74
nezinomas <nezinomas@gmai.com>
2024-05-08 13:59:21 +0300

    log last successful import

--- project/goals/management/commands/cron.py
+++ project/goals/management/commands/cron.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
 
 from ...lib.writer import Writer
+from ...models import Logs
 
 
 class Command(BaseCommand):
@@ -10,9 +11,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            Writer().write()
+            # Writer().write()
+            pass
         except Exception as e:
             raise CommandError(f"Can't sync with Strava - {e}")
+
+        Logs.objects.create()
 
         self.stdout.write(
             self.style.SUCCESS(f"{datetime.now()}: successfully get Strava leaderboard")
