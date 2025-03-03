bde229f371a84c3169061651c9603beebe86b937
Audrius <nezinomas@gmail.com>
2025-03-03 21:27:08 +0200

    formatted with ruff

--- project/goals/tests/test_models.py
+++ project/goals/tests/test_models.py
@@ -81,7 +81,9 @@ def test_activity_activities_stats_for_month_wrong_period():
     ActivityFactory()
     ActivityFactory(date=date(2022, 5, 1))
 
-    actual = Activities.objects.activities_stats(pendulum.date(2022, 4, 25), period="hack")
+    actual = Activities.objects.activities_stats(
+        pendulum.date(2022, 4, 25), period="hack"
+    )
 
     assert actual.count() == 1
     assert actual[0]["moving_time"] == 60
@@ -95,7 +97,9 @@ def test_activity_activities_stats_for_year():
     ActivityFactory()
     ActivityFactory(date=date(2023, 5, 1))
 
-    actual = Activities.objects.activities_stats(pendulum.date(2022, 1, 25), period="year")
+    actual = Activities.objects.activities_stats(
+        pendulum.date(2022, 1, 25), period="year"
+    )
 
     assert actual.count() == 1
     assert actual[0]["moving_time"] == 60
